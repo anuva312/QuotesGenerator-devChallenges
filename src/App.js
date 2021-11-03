2e56b49b6a20e6d8d76458902784123eb710101f
Anusree V A <anusreeva93@gmail.com>
2021-11-03 19:30:48 +0530

    Implemented fetching and displaying random quotes and quotes by author functionalities

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [randomQuote, setRandomQuote] = useState();
+  const [authorQuotes, setAuthorQuotes] = useState();
+
+  const generateRandomQuote = async function () {
+    setAuthorQuotes(false);
+    const url = "https://api.quotable.io/random";
+    try {
+      const response = await (await fetch(url)).json();
+      // console.log(response);
+      setRandomQuote(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const generateAuthorQuotes = async function () {
+    const url = `https://api.quotable.io/quotes?author=${randomQuote.author}`;
+    setRandomQuote(false);
+    try {
+      const response = await (await fetch(url)).json();
+      // console.log(response.results);
+      setAuthorQuotes(response.results);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    generateRandomQuote();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        className="App-header random-button"
+        onClick={() => generateRandomQuote()}
+      >
+        <span>random</span>
+        <span className="material-icons">autorenew</span>
+      </div>
+      {randomQuote ? (
+        <div>
+          <div>{randomQuote.content}</div>
+          <div
+            className="about-quote-container"
+            onClick={() => generateAuthorQuotes()}
+          >
+            <div>{randomQuote.author}</div>
+            <div>{randomQuote.tags.join(", ")}</div>
+          </div>
+        </div>
+      ) : authorQuotes ? (
+        <div>
+          {authorQuotes.map((quote) => (
+            <div>{quote.content}</div>
+          ))}
+        </div>
+      ) : (
+        <div>Spinner here</div>
+      )}
     </div>
   );
 }
